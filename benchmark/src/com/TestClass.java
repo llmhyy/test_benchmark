17ec8d6fe51301c255c78c694d65caf5b8468aab
Yun Lin <llmhyy@gmail.com>
2018-11-14 00:23:16 +0800

    interface test

---
+++ benchmark/src/com/TestClass.java
@@ -0,0 +1,12 @@
+package com;
+
+public class TestClass implements TestInterface {
+
+	private int a;
+	
+	@Override
+	public int getAttribute() {
+		return a;
+	}
+
+}
---
+++ benchmark/src/com/TestInterface.java
@@ -0,0 +1,5 @@
+package com;
+
+public interface TestInterface {
+	public int getAttribute();
+}
