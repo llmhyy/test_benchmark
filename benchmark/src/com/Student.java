a6c3ff8e1882026eab34f9d899fff5eaaceb9f7b
Yun Lin <llmhyy@gmail.com>
2018-11-13 21:00:11 +0800

    getter and setter

--- benchmark/src/com/Student.java
+++ benchmark/src/com/Student.java
@@ -14,6 +14,38 @@ public class Student {
 		this.male = male;
 		this.friend = friend;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public boolean isMale() {
+		return male;
+	}
+
+	public void setMale(boolean male) {
+		this.male = male;
+	}
+
+	public Student getFriend() {
+		return friend;
+	}
+
+	public void setFriend(Student friend) {
+		this.friend = friend;
+	}
 	
 	
 	
