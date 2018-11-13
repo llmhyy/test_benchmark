70e79158c1e2ed452c8049d5596466f5216b19a0
Yun Lin <llmhyy@gmail.com>
2018-11-13 19:29:24 +0800

    new benchmark

---
+++ benchmark/src/com/Student.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Student {
+	int id;
+	int score;
+	boolean male;
+	
+	Student friend;
+
+	public Student(int id, int score, boolean male, Student friend) {
+		super();
+		this.id = id;
+		this.score = score;
+		this.male = male;
+		this.friend = friend;
+	}
+	
+	
+	
+	
+}
